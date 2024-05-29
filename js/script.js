b1043e5c8a8d51329cb1d8d44adeec00d9760e5a
Erick670630 <grandeerick17@gmail.com>
2024-05-29 05:55:23 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('#carousel img');
+const slideCount = slides.length;
+
+function nextSlide() {
+    slides[currentSlide].style.display = 'none';
+    currentSlide = (currentSlide + 1) % slideCount;
+    slides[currentSlide].style.display = 'block';
+}
+
+setInterval(nextSlide, 3000);
